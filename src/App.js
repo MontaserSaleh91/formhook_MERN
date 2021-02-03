db0e049a8ac8d6f33e9a0563e9741b9326e2cc78
Montaser Alfrookh <montaser.alfrookh91@gmail.com>
2021-02-03 22:22:13 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+import React, { useState } from  'react';
 import './App.css';
+import FormComponent from './components/FormComponent';
+import ResultComponent from './components/ResultComponent.js';
+
 
 function App() {
+  const [state , setState] = useState({
+    firstName : "",
+    lastName : "",
+    email : "",
+    password : "",
+    confirmPassword : ""
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <FormComponent inputs = {state} setInputs = {setState} />
+      <ResultComponent data = {state} />
     </div>
   );
 }
